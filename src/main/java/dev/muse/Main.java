f878403d8f8dcb4ddfbc31c1cb952aecf3a39f9a
Matt Bauer <mbauer@galois.com>
2020-03-09 12:43:47 -0400

    update example

--- src/main/java/dev/muse/Main.java
+++ src/main/java/dev/muse/Main.java
@@ -7,8 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         ClassA classA = new ClassA();
-        //classA.calledMethodA();
-        classA.testEntrypoint(10);
+        classA.entryPoint();
 
 
 
