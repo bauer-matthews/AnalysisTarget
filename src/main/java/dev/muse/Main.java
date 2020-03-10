851fb3f99e170ad4a775b861afa7f3cf7bce6299
Matt Bauer <mbauer@galois.com>
2020-03-10 08:01:11 -0400

    added static method src/sink example to main

--- src/main/java/dev/muse/Main.java
+++ src/main/java/dev/muse/Main.java
@@ -9,9 +9,23 @@ public class Main {
         ClassA classA = new ClassA();
         classA.entryPoint();
 
-
-
         ClassB classB = new ClassB();
         classB.sourceMethod("very sensitive data");
+
+        mainSrc("A sensitive string");
+    }
+
+    private static void mainSrc(String userString) {
+        mainSink(userString);
+        mainSanitizer(userString);
+    }
+
+    private static void mainSink(String userString) {
+        System.out.println(userString);
+
+    }
+
+    private static void mainSanitizer(String userString) {
+        userString.replaceAll("/", ".");
     }
 }
